44fe20cfa39eaa0b5fecbe8fea7181ed766dc504
pyaesoekyaw <pyaesoekyaw1123@gmail.com>
2025-03-29 19:53:38 +0630

    add html and javascript file

---
+++ code_for_webpage/scripts.js
@@ -0,0 +1,57 @@
+// Add your API endpoint here
+var API_ENDPOINT = "https://rvn5eeps97.execute-api.us-east-1.amazonaws.com/StagePyae";
+
+// AJAX POST request to save student data
+document.getElementById("savestudent").onclick = function(){
+    var inputData = {
+        "studentid": $('#studentid').val(),
+        "name": $('#name').val(),
+        "class": $('#class').val(),
+        "age": $('#age').val()
+    };
+    $.ajax({
+        url: API_ENDPOINT,
+        type: 'POST',
+        data:  JSON.stringify(inputData),
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            document.getElementById("studentSaved").innerHTML = "Student Data Saved!";
+            $('#studentid').val('');  // Clear student ID field
+            $('#name').val('');       // Clear name field
+            $('#class').val('');      // Clear class field
+            $('#age').val('');  
+        },
+        error: function () {
+            alert("Error saving student data.");
+        }
+    });
+}
+
+// AJAX GET request to retrieve all students
+document.getElementById("getstudents").onclick = function(){  
+    $.ajax({
+        url: API_ENDPOINT,
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            // Sort the response data by studentid
+            response.sort((a, b) => a.studentid - b.studentid);
+
+            // Clear the table before adding new data
+            $('#studentTable tr').slice(1).remove();
+
+            // Append the sorted student data to the table
+            jQuery.each(response, function(i, data) {          
+                $("#studentTable").append("<tr> \
+                    <td>" + data['studentid'] + "</td> \
+                    <td>" + data['name'] + "</td> \
+                    <td>" + data['class'] + "</td> \
+                    <td>" + data['age'] + "</td> \
+                    </tr>");
+            });
+        },
+        error: function () {
+            alert("Error retrieving student data.");
+        }
+    });
+}
